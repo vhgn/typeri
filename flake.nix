cfc7255078268ad07de0fbb463bf2acf2880f75b
Vahagn <metavahagn@icloud.com>
2025-05-02 19:08:23 +0400

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "Development Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default =
+        pkgs.mkShell
+          {
+            buildInputs = with pkgs; [
+              elixir
+			  erlang
+            ];
+          };
+    };
+}
